291576f3aa51e441774f79c7185f6df9c5107cee
Piyush Singh <workspace.piyush01@gmail.com>
2025-08-14 20:06:07 +0530

    implemented change message model in domain/models

---
+++ src/Domain/Models/ChangeMessage.cs
@@ -0,0 +1,10 @@
+namespace Domain.Models;
+
+public sealed class ChangeMessage
+{
+    public required string Collection { get; init; }
+    public required string OperationType { get; init; } // insert|update|replace|delete
+    public string? Id { get; init; }
+    public object? Document { get; init; }
+    public DateTime TimestampUtc { get; init; } = DateTime.UtcNow;
+}
