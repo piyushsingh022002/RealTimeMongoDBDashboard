6f9d4b31eaeda674f8df32a2c1d3f1da6ebf8265
Piyush Singh <workspace.piyush01@gmail.com>
2025-08-18 11:17:53 +0530

    client added

--- src/Api/Hubs/NotificationsHub.cs
+++ src/Api/Hubs/NotificationsHub.cs
@@ -4,5 +4,9 @@ namespace RealTimeMongoDashboard.API.Hubs
 {
     public class NotificationsHub : Hub
     {
+        public async Task SendNotification(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
     }
 }
