7a3e8ff99534396dc325a4368063169b9e43ea89
Piyush Singh <workspace.piyush01@gmail.com>
2025-08-18 11:23:05 +0530

    method added for test

--- src/Api/Hubs/NotificationsHub.cs
+++ src/Api/Hubs/NotificationsHub.cs
@@ -4,6 +4,12 @@ namespace RealTimeMongoDashboard.API.Hubs
 {
     public class NotificationsHub : Hub
     {
+
+         public override async Task OnConnectedAsync() //optional just to check if the user is connected to the hubs
+        {
+            await Clients.Caller.SendAsync("ReceiveMessage", "Connected to NotificationsHub");
+            await base.OnConnectedAsync();
+        }
         public async Task SendNotification(string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", message);
